89de495424ecf1e6d2ab3b5ed5bf61f051f9800e
sfomuseumbot <sfomuseumbot@localhost>
2024-07-05 11:51:19 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,36 @@
+# go-guardian-liveblog
+
+Go package for watching Guardian live blog events and reading them aloud using the operating system's text-to-speech APIs.
+
+## Important
+
+This is MacOS specific right now.
+
+## Tools
+
+
+### follow
+
+```
+> ./bin/follow -h
+Usage of ./bin/follow:
+  -delay int
+    	The number of seconds to wait before fetching new updates (default 30)
+  -url string
+    	The URL of the event being live blogged
+```
+
+For example:
+
+```
+$> ./bin/follow -url https://www.theguardian.com/football/live/2024/jul/05/spain-v-germany-euro-2024-quarter-final-live-score-updates
+
+2024/07/05 11:42:50 INFO It’s finally all over in Stuttgart, where Spain advance to the semi-finals at the expense of the hosts courtesy of Mikel Merino’s late, late, late winner. Football, bloody hell.
+2024/07/05 11:43:01 INFO ET30+6: The Spanish right-back walks for a second yellow after wrapping his hands around the neck of Musiala in a bid to bring him down. Free kick for Germany. Toni Kroos takes his last kick of the ball as a professional footballer and Germany are out …
+2024/07/05 11:43:16 INFO ET30+4: Fulkrug heads just wide from seven or eight yards after a Muller cross came his way. That was the chance!!!
+2024/07/05 11:43:24 INFO ET30+2: Kimmich has a shot from outside the area blocked. Ferran Torres breaks upfield but elects to try to score instead of running to the corner flag. Neuer launches the ball forward.
+...and so on
+```
+
+The `follow` tool will keep a local cache of posts its already seen (and read) for the duration it is run.
+
---
+++ cmd/follow/main.go
@@ -0,0 +1,97 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	"log/slog"
+	"os/exec"
+	"strings"
+	"sync"
+	"time"
+
+	"github.com/anaskhan96/soup"
+)
+
+func main() {
+
+	var url string
+	var delay int
+
+	flag.StringVar(&url, "url", "", "The URL of the event being live blogged")
+	flag.IntVar(&delay, "delay", 30, "The number of seconds to wait before fetching new updates")
+
+	flag.Parse()
+
+	ctx := context.Background()
+
+	cache := new(sync.Map)
+
+	handle_posts(ctx, url, cache)
+
+	ticker := time.NewTicker(time.Duration(delay) * time.Second)
+	defer ticker.Stop()
+
+	for {
+		select {
+		case <-ticker.C:
+			handle_posts(ctx, url, cache)
+		}
+	}
+
+}
+
+func read_post(ctx context.Context, post string) {
+	slog.Info(post)
+	cmd := exec.Command("say", post)
+	cmd.Run()
+}
+
+func handle_posts(ctx context.Context, url string, cache *sync.Map) {
+
+	posts, err := get_posts(ctx, url)
+
+	if err != nil {
+		slog.Error("Failed to retrieve posts", "url", url, "error", err)
+		return
+	}
+
+	for _, p := range posts {
+
+		_, exists := cache.LoadOrStore(p, true)
+
+		if exists {
+			continue
+		}
+
+		read_post(ctx, p)
+	}
+}
+
+func get_posts(ctx context.Context, url string) ([]string, error) {
+
+	slog.Info("Get posts", "url", url)
+	rsp, err := soup.Get(url)
+
+	if err != nil {
+		return nil, fmt.Errorf("Failed to retrieve %s, %w", url, err)
+	}
+
+	posts := make([]string, 0)
+
+	doc := soup.HTMLParse(rsp)
+	paras := doc.FindAll("p")
+
+	for _, p := range paras {
+
+		p_class := p.Attrs()["class"]
+
+		if !strings.HasPrefix(p_class, "dcr-") {
+			continue
+		}
+
+		posts = append(posts, p.FullText())
+	}
+
+	return posts, nil
+}
